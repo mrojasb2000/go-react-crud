4616efedcff07405e5515cb6a31a43ca658a76b4
maalroja <maalroja@bancolombia.com.co>
2023-09-07 17:11:22 -0300

    chore: add first users endpoint

--- main.go
+++ main.go
@@ -4,11 +4,23 @@ import (
 	"fmt"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
 	app := fiber.New()
+
+	// Initialize default config
+	app.Use(cors.New())
+
 	app.Static("/", "./public")
+
+	app.Get("/users", func(c *fiber.Ctx) error {
+		return c.JSON(&fiber.Map{
+			"message": "users list from backend",
+		})
+	})
+
 	app.Listen(":3000")
 	fmt.Println("Server is running on port :3000")
 }
