edb5018f7f6631adc44c219532d460d8bb8646ea
maalroja <maalroja@bancolombia.com.co>
2023-09-08 11:31:58 -0300

    chore: add create new user and posts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 function App() {
+ 
   return (
     <>
       <div>Hello World</div>
@@ -7,6 +8,18 @@ function App() {
         const data = await response.json()
         console.log(data)
       }}>Get users</button>
+      <button onClick={async () => {
+      const response = await fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({name: "John Doe"})
+        }).then(response => response.json())
+        .then(response => console.log(response));
+        console.log(response);
+      }}>Create User</button>
     </>
   )
 }
--- main.go
+++ main.go
@@ -1,14 +1,28 @@
 package main
 
 import (
+	"context"
 	"fmt"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+const dBName = "react-crud"
+const mongoDbUrl = "mongodb://localhost:27017/" + dBName
+
 func main() {
+
 	app := fiber.New()
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(mongoDbUrl))
+	if err != nil {
+		panic(err)
+	}
+
+	coll := client.Database(dBName).Collection("users")
 
 	// Initialize default config
 	app.Use(cors.New())
@@ -18,6 +32,21 @@ func main() {
 	app.Get("/users", func(c *fiber.Ctx) error {
 		return c.JSON(&fiber.Map{
 			"message": "users list from backend",
+		})
+	})
+
+	app.Post("/users", func(c *fiber.Ctx) error {
+		_, err := coll.InsertOne(
+			context.TODO(),
+			bson.D{
+				{Key: "name", Value: "Mavro"},
+			},
+		)
+		if err != nil {
+			panic(err)
+		}
+		return c.Status(201).JSON(&fiber.Map{
+			"message": "User created",
 		})
 	})
 
